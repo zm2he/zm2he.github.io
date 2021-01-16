a4a3b1a5c85a7c33c80af012da379d44160f4bcc
brucehe <bruce.he.62@gmail.com>
2021-01-16 13:34:00 -0500

    uploaded play3

---
+++ play3/precache-manifest.35361b90004bb90bc7f74625efaf7598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0691f374048c2a10d02c75dc4ec45961",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93c5eb65b263f8c1ce50",
+    "url": "./static/css/2.ef440c72.chunk.css"
+  },
+  {
+    "revision": "725a5d88a4e530219966",
+    "url": "./static/css/main.3a52b3a1.chunk.css"
+  },
+  {
+    "revision": "93c5eb65b263f8c1ce50",
+    "url": "./static/js/2.de768d56.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.de768d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725a5d88a4e530219966",
+    "url": "./static/js/main.3f6fbe66.chunk.js"
+  },
+  {
+    "revision": "6275f8d7d08a74fd2bd3",
+    "url": "./static/js/runtime-main.32d44af4.js"
+  }
+]);
